ccbabd3e3c4ce8cb0cc3788c27bd1a98e9ef3a53
Johannes Plapp <johannes.plapp@logivations.com>
2025-12-30 00:54:23 +0100

    .

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false, // Set to true if using HTTPS
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   }
 }));
